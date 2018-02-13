b816798660acd90ea20a05ba9f8438b113748436
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-13 18:03:08 +0000

    Melhorando layout

--- resources/views/adicionado.blade.php
+++ resources/views/adicionado.blade.php
@@ -2,6 +2,8 @@
 
 @section('conteudo')
 
-Produto {{$nome}} adicionado com sucesso!
+<div class="alert alert-success">
+    <strong>Sucesso!</strong> O produto {{$nome}} foi adicionado.
+</div>
 
 @stop
