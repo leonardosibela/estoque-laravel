3555fbae57aef50c3be92423b02f90c0ec497f61
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-13 16:13:31 +0000

    Estilizando o template principal

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,13 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <title>Título da página</title>
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
+    <title>Controle de estoque</title>
 </head>
 <body>
+  <div class="container">
 
-@yield('conteudo')
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
 
+    <div class="navbar-header">      
+      <a class="navbar-brand" href="/produtos">Estoque Laravel</a>
+    </div>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/produtos">Listagem</a></li>
+      </ul>
+
+    </div>
+  </nav>
+
+  <br>
+
+    @yield('conteudo')
+
+  <footer class="footer">
+      <p>© Curso de Laravel inicante</p>
+  </footer>
+
+  </div>
 </body>
 </html>
