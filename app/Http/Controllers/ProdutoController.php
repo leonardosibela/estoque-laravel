770fa11ee1a1fb3823bdc3a6bc639f522b022577
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-14 13:01:24 +0000

    Usando o Eloquent model para criar um ORM de Produto e assim criar a camada de modelo

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\DB;
 use Request;
+use App\Produto;
 
 class ProdutoController extends Controller {
 
     public function lista() {
-        $produtos = DB::select('select * from produtos');
+        $produtos = Produto::all();
 
         return view('produto.listagem')->with('produtos', $produtos);
     }
