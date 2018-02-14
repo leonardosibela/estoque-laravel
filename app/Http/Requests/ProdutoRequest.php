1bebbfc1d4e9294f0496644f58999c65311c92c8
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-14 18:06:31 +0000

    Customizando mensagens de error do FormRequest

--- app/Http/Requests/ProdutoRequest.php
+++ app/Http/Requests/ProdutoRequest.php
@@ -18,4 +18,18 @@ class ProdutoRequest extends FormRequest {
             'tamanho' => 'required|max:100'
         ];
     }
+
+    public function messages() {
+        return [
+            'nome.required' => 'O campo nome é obrigatório',
+            'valor.required' => 'O campo valor é obrigatório',
+            'quantidade.required' => 'O campo quantidade é obrigatório',
+            'tamanho.required' => 'O campo tamanho é obrigatório',
+            'nome.min' => 'O campo nome precisa ter no mínimo 2 caracteres',
+            'descricao.max' => 'O campo descriçãoo não pode ter mais do que 255 caracteres',
+            'tamanho.max' => 'O campo tamanho não pode ter mais do que 100 caracteres',
+            'valor.numeric' => 'O campo valor precisa ser numérico',
+            'quantidade.numeric' => 'O campo quantidade precisa ser numérico'
+        ];
+    }
 }
